6f7eecbfca81f40d07ad6430394a99b4c47b6e15
임우택/선임연구원/VC AVN1개발담당(wootaek.lim@lge.com) <wootaek.lim@lge.com>
2018-02-26 14:00:34 +0900

    BOJ #2965 : 캥거루 세마리 문제

---
+++ BOJ/src/com/lwt/boj/step16/BOJ2965.java
@@ -0,0 +1,25 @@
+package com.lwt.boj.step16;
+
+import java.util.Scanner;
+
+public class BOJ2965 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		int C = sc.nextInt();
+		sc.close();
+		int left = Math.abs(A - B);
+		int right = Math.abs(B - C);
+		int result = 0;
+		int temp = B;
+		
+		if (left < right) {
+			result = C - temp - 1; 
+		} else {
+			result = temp - A - 1;
+		}
+		
+		System.out.println(result);
+	}
+}

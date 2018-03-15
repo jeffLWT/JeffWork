18e82038882b0fcd1953733ea8248a839e3ee30f
임우택/선임연구원/VC AVN1개발담당(wootaek.lim@lge.com) <wootaek.lim@lge.com>
2018-03-15 16:04:35 +0900

    [DP] BOJ1010 Using DP not Combination

--- BOJ/src/com/lwt/boj/dp/Main1010.java
+++ BOJ/src/com/lwt/boj/dp/Main1010.java
@@ -8,35 +8,32 @@ public class Main1010 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
+		int[][] dp;
 		
 		while (T > 0) {
 			count = 0;
 			int N = sc.nextInt();
 			int M = sc.nextInt();
-			combination(M, N, 0, count);
-			System.out.println(count);
+			dp = new int[N + 1][M + 1];
+			
+			for (int i = 1;i <= M;i++) {
+				dp[1][i] = i;
+			}
+			
+			for (int i = 2;i <= N;i++) {
+				for (int j = i;j <= M;j++) {
+					if (i == j) {
+						dp[i][j] = 1;
+					} else {
+						dp[i][j] = dp[i - 1][j - 1] + dp[i][j - 1];
+					}
+				}
+			}
+			
+			System.out.println(dp[N][M]);
 			T--;
 		}
 		
 		sc.close();
-	}
-
-	private static void combination(int m, int n, int target, int c) {
-		if (n == 0) {
-			//System.out.println("n == 0");
-			count += 1;
-		} else if (m == n) {
-			//System.out.println("m == n");
-			count += 1;
-		} else if (m == target) {
-			//System.out.println("m == target");
-			return;
-		} else {
-			//System.out.println("1 m = " + m + " n = " + n + " target = " + target + " count = " + count);
-			combination(m, n - 1, target + 1, count + 1);
-			//System.out.println("2 m = " + m + " n = " + n + " target = " + target + " count = " + count);
-			combination(m, n, target + 1, count + 1);
-			//System.out.println("3 m = " + m + " n = " + n + " target = " + target + " count = " + count);
-		}
 	}
 }

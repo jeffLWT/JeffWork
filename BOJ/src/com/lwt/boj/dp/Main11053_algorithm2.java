75d9d2d45da925776c9bae0bf78f45eb5ca277d7
임우택/선임연구원/VC AVN1개발담당(wootaek.lim@lge.com) <wootaek.lim@lge.com>
2018-03-15 14:21:20 +0900

    [DP] LIS by using O(NlongN) Algorithm

---
+++ BOJ/src/com/lwt/boj/dp/Main11053_algorithm2.java
@@ -0,0 +1,44 @@
+package com.lwt.boj.dp;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Using O(NlogN) Algorithm
+ */
+public class Main11053_algorithm2 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] A = new int[N + 1];
+		
+		for (int i = 1;i <= N;i++) {
+			A[i] = sc.nextInt();
+		}
+		
+		System.out.println(getLISLength(A, N));
+		sc.close();
+	}
+
+	private static int getLISLength(int[] arr, int size) {
+		int length = 0;
+		int[] list = new int[size];
+		list[0] = arr[1];
+		length = 1;
+		
+		for (int i = 2;i <= size;i++) {
+			if (arr[i] < list[0]) { // 입력된 값이 list 의 index = 0 값보다 작을 경우
+				list[0] = arr[i];
+			} else if (list[length - 1] < arr[i]) { // 입력된 값이 list의 마지막 값보다 클 경우
+				list[length] = arr[i];
+				length++;
+			} else {
+				int index = Arrays.binarySearch(arr, 0, length, arr[i]);
+				index = (index < 0) ? -index - 1 : index;
+				list[index] = arr[i];
+			}
+		}
+		
+		return length;
+	}
+}
